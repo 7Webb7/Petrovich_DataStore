393d622daec5030bd8266ccdf52a8ee1744217da
7Webb7 <v.dovybortsev@gmail.com>
2024-06-07 08:45:12 +0500

    New Files

--- venv/main.py
+++ venv/main.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import telebot
 import datetime
+import numpy as np
 
 with open('bot_token.txt', 'r', encoding='utf-8') as file:
     token = file.read()
 
 data = pd.read_csv('petrovich_log.csv')
 df = pd.DataFrame(data)
-
+known_commands = ['/start', '/messages', '/help']
 bot = telebot.TeleBot(token)
-@bot.message_handler(commands=['/start'])
+@bot.message_handler(commands=['start'])
 def start_handle(message):
     bot.send_message(message.chat.id, 'Привет, меня зовут Петрович. Нужно что?')
+@bot.message_handler(commands =['messages'])
+def print_messages(message):
+    to_print = np.array(df.loc[:, ['Message']])
+    to_print = np.unique(to_print)[-10:]
+
+    for i in range(len(to_print)):
+
+        bot.send_message(message.chat.id, f"{i+1}. {to_print[i]}")
+
 @bot.message_handler(content_types= 'text')
 def storing_handle(message):
+    if message.text in known_commands:
+        return
     global df
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     df.loc[len(df.index)] = [message.from_user.id, timestamp, message.message_id, message.text]
