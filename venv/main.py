9f5b3041f58fb917338753c9b672d8a96e2a1953
7Webb7 <v.dovybortsev@gmail.com>
2024-06-07 11:58:33 +0500

    New Files

--- venv/main.py
+++ venv/main.py
@@ -11,15 +11,19 @@ data = pd.read_csv('petrovich_log.csv')
 df = pd.DataFrame(data)
 known_commands = ['/start', '/messages', '/help']
 bot = telebot.TeleBot(token)
+
 @bot.message_handler(commands=['start'])
 def start_handle(message):
     bot.send_message(message.chat.id, 'Привет, меня зовут Петрович. Нужно что?')
-#@bot.message_handler(commands =['messages'])
-#def print_messages(message):
+
+
 @bot.message_handler(commands = ['messages'])
 def handle_show_messages(message):
+    global df
     unique_messages = {}
     for index, row in df[::-1].iterrows():
+        if row['Message'].startswith('/'):
+            continue
         if row['Message'] in unique_messages:
             continue
         unique_messages[row['Message']] = {'UserID': row['UserID'], 'Username': row['Username']}
@@ -31,17 +35,43 @@ def handle_show_messages(message):
     for messages, user in unique_messages.items():
         message_string += f'Пользователь: {user['Username']}\t ID: {user['UserID']} \n\nСообщение: {messages} \n\n\n'
     bot.send_message(message.chat.id, message_string)
+    df.loc[len(df.index)] = [message.from_user.username, message.from_user.id, '/messages']
+    df.to_csv('petrovich_log.csv', index=False)
 
+@bot.message_handler(commands = ['getcommands'])
+def handle_commands(message):
+    global df
+    commands = {}
 
+    for index, row in df[::-1].iterrows():
 
-@bot.message_handler(content_types= 'text')
+        if not row['Message'].startswith('/'):
+            continue
+
+        commands[index] = {'UserID': row['UserID'], 'Username': row['Username'], 'Message': row['Message']}
+
+        # Если набрали 10 уникальных сообщений, выходим из цикла
+        if len(commands) == 10:
+            break
+    message_string = ""
+    for messages, user in commands.items():
+        message_string += f'Пользователь: {user['Username']}\t ID: {user['UserID']} \n\nИспользовал: {user['Message']} \n\n\n'
+    bot.send_message(message.chat.id, message_string)
+
+    df.loc[len(df.index)] = [message.from_user.username, message.from_user.id, '/getcommands']
+    df.to_csv('petrovich_log.csv', index=False)
+
+@bot.message_handler(content_types= ['text', 'commands'])
 def storing_handle(message):
-    if message.text in known_commands or message.text.startswith('/'):
+    if message.text.startswith('/'):
         return
     global df
 
     df.loc[len(df.index)] = [message.from_user.username, message.from_user.id, message.text]
     df.to_csv('petrovich_log.csv', index=False)
+
+
+
     bot.reply_to(message, 'Сообщение записано.')
 
 
